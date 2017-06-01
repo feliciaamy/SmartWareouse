512705ebf214f61f57ab6f057e77835583df1704
Ryan <ryanlim43@hotmail.com>
2017-06-01 22:17:54 +0800

    Amy updation of dimension

--- app/src/main/java/org/boofcv/android/localization/Dimension.java
+++ app/src/main/java/org/boofcv/android/localization/Dimension.java
@@ -17,6 +17,7 @@ public class Dimension {
     Orientation orientation;
     Shape shape;
 
+    // Rectangle
     public Dimension(double left, double top, double right, double bottom, int color) {
         this.left = left;
         this.top = top;
@@ -26,14 +27,16 @@ public class Dimension {
         this.shape = Shape.RECTANGLE;
     }
 
+    // Circle
     public Dimension(double x, double y, int r, int color) {
         this.r = r;
-        this.shape = Shape.CIRCLE;
         this.x = x;
         this.y = y;
         this.color = color;
+        this.shape = Shape.CIRCLE;
     }
 
+    // Line
     public Dimension(double start, double end, double center, Orientation orientation, int color) {
         this.start = start;
         this.end = end;
@@ -44,6 +47,78 @@ public class Dimension {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Dimension dimension = (Dimension) o;
+
+        if (this.shape != ((Dimension) o).shape) {
+            return false;
+        }
+        double eps = 0;
+        switch (this.shape) {
+            case LINE:
+                eps = Math.abs(this.start - this.end) * 0.2;
+                if (this.orientation != ((Dimension) o).orientation) {
+                    return false;
+                }
+
+                if (Math.abs(this.start - ((Dimension) o).start) < eps &&
+                        Math.abs(this.end - ((Dimension) o).end) < eps &&
+                        Math.abs(Math.abs(this.start - this.end) - Math.abs(((Dimension) o).start - ((Dimension) o).end)) < eps &&
+                        Math.abs(this.center - ((Dimension) o).center) < eps) {
+                    return true;
+                } else {
+                    return false;
+                }
+            case CIRCLE:
+                if (Math.abs(this.x - ((Dimension) o).x) < r && (Math.abs(this.x - ((Dimension) o).x) < r)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            case RECTANGLE:
+                eps = Math.sqrt(Math.pow((this.left - this.right), 2) + Math.pow((this.top - this.bottom), 2)) / 2;
+                if (Math.sqrt(Math.pow((this.left - this.right), 2) + Math.pow((this.top - this.bottom), 2)) < eps) {
+                    return true;
+                } else
+                    return false;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(left);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(top);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(right);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(bottom);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + r;
+        temp = Double.doubleToLongBits(x);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(start);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(end);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(center);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + color;
+        result = 31 * result + (orientation != null ? orientation.hashCode() : 0);
+        result = 31 * result + (shape != null ? shape.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         switch (shape) {
             case LINE:
@@ -51,6 +126,7 @@ public class Dimension {
             case CIRCLE:
                 return "Circle: " + "(" + x + ", " + y + "), radius = " + r;
             case RECTANGLE:
+
                 return "Rectangle: " + "(" + left + ", " + top + "), (" + right + ", " + bottom + ")";
         }
         return "";
